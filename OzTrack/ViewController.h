6630f1e169a2cdeb87c43b105029fdf74e2b5791
Madan Parameswaran <madan.mesh@gmail.com>
2015-10-10 10:10:20 +1030

    Initial Commit

---
+++ OzTrack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OzTrack
+//
+//  Created by Madan on 10/10/15.
+//  Copyright (c) 2015 Unearthed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OzTrack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OzTrack
+//
+//  Created by Madan on 10/10/15.
+//  Copyright (c) 2015 Unearthed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
